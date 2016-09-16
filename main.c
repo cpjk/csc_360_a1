29008decf6e65386c0528d07a419e10a43223722
Chris Kelly <cpjkelly@gmail.com>
2016-09-16 13:28:23 -0700

    Add create_process

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define MAX_INPUT_LENGTH 500
 
-char* prompt_and_accept_input() {
+char *prompt_and_accept_input();
+char *pstat();
+int create_process();
+
+char *prompt_and_accept_input() {
   char *input_buffer = malloc(sizeof(char) * MAX_INPUT_LENGTH);
 
   printf("Pman: >");
@@ -15,11 +21,36 @@ char* prompt_and_accept_input() {
   return input_buffer;
 }
 
+int create_process(char **args) {
+  int pid = fork();
+  if(pid  < 0) {
+    printf("Fork failed");
+    return -1;
+  }
+  else if(pid > 0) {
+    return pid;
+  }
+  else {
+    execvp(*args, args);
+  }
+}
+
+char *pstat(char *pid) {
+  return "";
+}
+
 int main(int argc, char **argv) {
-  char *input;
+  /* /1* char *args[] = {"./inf", "1", "2"}; *1/ */
+  /* /1* char *args[] = {"./print1"}; *1/ */
+  /* /1* int pid = create_process("./inf", args); *1/ */
+  /* int pid = create_process(args); */
+  /* sleep(5); */
+  /* kill(pid, 15); */
   while(1) {
+    char *input;
     input = prompt_and_accept_input();
     printf(input);
+    free(input);
     printf("\n");
   }
 }
